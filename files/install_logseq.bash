3acbd6347cff87f5174705e97f9d0e9edcce3873
Sean Pascoe <sean.pascoe.mastercard.com>
2023-01-03 16:46:21 -0700

    make it work

--- files/install_logseq.bash
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-set -e
-
-[ ! -f .env ] || export $(grep -v '^#' .env | xargs)
-gh release download -R logseq/logseq -p '*.AppImage' -O ~/.local/bin/logseq
-chmod u+x "$HOME/.local/bin/logseq"
